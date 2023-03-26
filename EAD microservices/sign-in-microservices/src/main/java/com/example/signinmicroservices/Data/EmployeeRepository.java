210b4f5c7d2616cd5981b61595229c839422f551
sureshhansaka <sureshhansaka@gmail.com>
2023-03-26 16:01:03 +0530

    sign in microservics

---
+++ EAD microservices/sign-in-microservices/src/main/java/com/example/signinmicroservices/Data/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.example.signinmicroservices.Data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+    @Query("select e from Employee e where e.username=?1 and e.password=?2")
+    public List<Employee> findEmployeeByUsernameAndPassword(String username,String password);
+}
---
+++ EAD microservices/sign-in-microservices/src/main/java/com/example/signinmicroservices/Service/EmployeeService.java
@@ -0,0 +1,27 @@
+package com.example.signinmicroservices.Service;
+
+import com.example.signinmicroservices.Data.Employee;
+import com.example.signinmicroservices.Data.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    public List<Employee> getAllEmployee(){
+        List<Employee> employees=employeeRepository.findAll();
+        return employees;
+    }
+    public Employee createEmployee(Employee employee){
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> findEmployeeByUsernameAndPassword(String username,String password){
+        return employeeRepository.findEmployeeByUsernameAndPassword(username,password);
+    }
+
+}
